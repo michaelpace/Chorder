2ce7c4a4068316f2788542eb8257ebd8cf784b5a
Michael Pace <mpace1027@gmail.com>
2017-04-15 23:40:03 -0400

    really hacky tokenizer working

--- Chorder/main.swift
+++ Chorder/main.swift
@@ -212,20 +212,34 @@ extension Character {
 
  */
 
-enum Token {
-    case letter
-    case number
+enum Token: Equatable {
+    case letter(value: String)
+    case number(value: String)
+    case additionalNumbers(value: String)
     case slash
 
     init?(character: Character) {
         if character.isLetter {
-            self = .letter
+            self = .letter(value: String(character))
         } else if character.isNumeric {
-            self = .number
+            self = .number(value: String(character))
         } else if character.isSlash {
             self = .slash
         } else {
             return nil
+        }
+    }
+
+    // MARK: Equatable
+
+    static func == (lhs: Token, rhs: Token) -> Bool {
+        switch (lhs, rhs) {
+        case (let .letter(value: lhsValue), let .letter(value: rhsValue)), (let .number(value: lhsValue), let .number(value: rhsValue)), (let .additionalNumbers(value: lhsValue), let .additionalNumbers(value: rhsValue)):
+            return lhsValue == rhsValue
+        case (.slash, .slash):
+            return true
+        default:
+            return false
         }
     }
     
@@ -248,11 +262,48 @@ struct TokenSequence {
 struct Tokenizer {
 
     static func tokenize(string: String) -> TokenSequence? {
-        let tokens = string.characters.flatMap(Token.init)
+        let characters = string.characters
+
+        var tokens = [Token]()
+        var index = string.characters.startIndex
+
+        while index != characters.endIndex {
+
+            let character = characters[index]
+            guard let token = Token(character: characters[index]) else { assertionFailure("Encountered unexpected character: \(character)"); break }
+            tokens.append(token)
+
+            // Only continue beyond this guard to parse additional numbers if this one was a number.
+            guard character.isNumeric else { index = characters.index(after: index); continue }
+
+            var nextIndex = characters.index(after: index)
+            var numbersSeen = String()
+
+            while nextIndex != characters.endIndex {
+                let nextCharacter = characters[nextIndex]
+                guard nextCharacter.isNumeric else { break }
+
+                numbersSeen.append(nextCharacter)
+                nextIndex = characters.index(after: nextIndex)
+            }
+
+            guard numbersSeen.characters.count > 0 else { index = characters.index(after: index); continue }
+
+            tokens.append(.additionalNumbers(value: numbersSeen))
+            index = nextIndex
+        }
+
         return TokenSequence(with: tokens)
     }
 
 }
+
+// Test code. TODO: Remove.
+//let strings = ["1", "m164", "b1", "b76", "7/5", "443/7"]
+//strings.forEach { string in
+//    let tokenSequence = Tokenizer.tokenize(string: string)
+//    print("TokenSequence for \(string): \(tokenSequence)")
+//}
 
 struct Parser {
 
